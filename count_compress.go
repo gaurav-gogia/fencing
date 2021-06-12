5eef9279c025b96fdebad52bb682561054845d15
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2021-06-12 18:41:38 +0530

    Create count_compress.go

---
+++ count_compress.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"strconv"
+)
+
+func count_compress(input string) []rune {
+	var count, i, j int
+	runarry := []rune(input)
+	length := len(runarry)
+
+	for i = 0; i < length; i++ {
+		count = 1
+
+		for j = i + 1; j < length; j++ {
+
+			if runarry[i] != runarry[j] {
+				break
+			}
+
+			count++
+		}
+		countStr := strconv.Itoa(count)
+		runarry = append(runarry, runarry[i])
+		runarry = append(runarry, []rune(countStr)...)
+
+		i = j - 1
+	}
+
+	return runarry[i:]
+}

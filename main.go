67216d240dd555be85a69093b9e9a7c333994ba5
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2021-06-26 04:47:38 +0530

    print full and compressed array

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ func main() {
 	fmt.Println("Longest Substring: ")
 	fmt.Printf("%s: %d\n\n", longest_sub, len(longest_sub))
 
+	fmt.Println("Full Array: ")
+	fmt.Println(string(input))
+	fmt.Println()
+
 	compressed := count_compress(input)
 	fmt.Println("Compressed Array: ")
 	fmt.Println(string(compressed))
